1c145beb429b50895bffe9e6405ebf610c1c9209
victorBR116 <65189745+victorBR116@users.noreply.github.com>
2022-11-17 15:44:24 -0300

    att

--- leitorZip.php
+++ leitorZip.php
@@ -1,3 +1,8 @@
 <?php
 
-echo file_get_contents('zip://arquivos.zip#Filmes.txt');
+$contexto = stream_context_create([
+    'zip' => [
+        'password' => '123'
+    ]
+]);
+echo file_get_contents('zip://arquivos.zip#Filmes.txt', false, $contexto);
